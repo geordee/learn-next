7202d8dab828a8ef1b06b0c7ce8bcaf4fc375c7b
Geordee Naliyath <geordee@gmail.com>
2020-04-17 11:04:03 +0400

    Styled children

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,22 @@ const ShowLink = ({ show }) => (
     <Link href="/p/[id]" as={`/p/${show.id}`}>
       <a>{show.name}</a>
     </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: "Arial";
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </li>
 );
 
@@ -26,20 +42,6 @@ const Index = (props) => (
 
       ul {
         padding: 0;
-      }
-
-      li {
-        list-style: none;
-        margin: 5px 0;
-      }
-
-      a {
-        text-decoration: none;
-        color: blue;
-      }
-
-      a:hover {
-        opacity: 0.6;
       }
     `}</style>
   </Layout>
