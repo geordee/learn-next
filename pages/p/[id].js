1f7b2c432ade668ac4324309fa6ccfe38dc2cf7c
Geordee Naliyath <geordee@gmail.com>
2020-04-17 11:12:19 +0400

    Global Styles

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,11 +1,25 @@
+import Markdown from "react-markdown";
 import Layout from "../../components/MyLayout";
 import fetch from "isomorphic-unfetch";
 
 const Post = (props) => (
   <Layout>
-    <h1>{props.show.name}</h1>
+    <div className="markdown">
+      <Markdown source={`# ${props.show.name}`} />
+    </div>
     <p>{props.show.summary.replace(/<[/]?[pb]>/g, "")}</p>
     {props.show.image ? <img src={props.show.image.medium} /> : null}
+
+    <style jsx global>{`
+      .markdown {
+        font-family: "Arial";
+      }
+
+      .markdown h1 {
+        padding: 0;
+        text-transform: uppercase;
+      }
+    `}</style>
   </Layout>
 );
 
